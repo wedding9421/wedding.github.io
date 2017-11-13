e9de9d4f13ae31bcfff258fbf637f83a80e05010
wedding9421 <33616673+wedding9421@users.noreply.github.com>
2017-11-13 18:18:52 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,16 +18,17 @@
 
 	};
 	
-function NextFrag(){
 
- if (index < URLArray.length)
- {
-   $("#VideoContainer").html('<video  id="video1" controls autoplay width="100%"> "<source src= "'+ URLArray[index]+ '" type="video/mp4"></source> </video>' );
-   index++;
-   $("#video1").bind( "ended", NextFrag);
- }
-}
 
+// get the video
+var video = document.querySelector('video');
+// use the whole window and a *named function*
+window.addEventListener('touchstart', function videoStart() {
+  video.play();
+  console.log('first touch');
+  // remove from the window and call the function we are removing
+  this.removeEventListener('touchstart', videoStart);
+});
 
 	var offcanvasMenu = function() {
 
