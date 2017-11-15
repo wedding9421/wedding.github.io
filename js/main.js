8afcbd67a97389fd6d533f0bd260b9d0c2a2071c
wedding9421 <33616673+wedding9421@users.noreply.github.com>
2017-11-15 18:11:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,24 +118,9 @@
 		} , { offset: '85%' } );
 	};
 	
-	 window.addEventListener('load', function () {
-        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        var source = audioCtx.createBufferSource();
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'music/1.mp3');
-        xhr.responseType = 'arraybuffer';
-        xhr.addEventListener('load', function (r) {
-            audioCtx.decodeAudioData(
-                    xhr.response, 
-                    function (buffer) {
-                        source.buffer = buffer;
-                        source.connect(audioCtx.destination);
-                        source.loop = false;
-                    });
-            source.start(0);
-        });
-        xhr.send();
-    });
+	sound = document.createElement('audio');
+sound.setAttribute('src', 'music/1.mp3');
+sound.play();
 
 
 	var dropdown = function() {
